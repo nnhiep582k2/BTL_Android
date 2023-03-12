860244cc769215f7b1e49cfea1bddfb1e2053a2e
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-03-12 10:31:54 +0700

    feat: add some rules

---
+++ app/src/main/java/com/group1/tourmanager/guidelines/Guidelines.java
@@ -0,0 +1,33 @@
+package com.group1.tourmanager.guidelines;
+
+public class Guidelines {
+    /**
+     * @job:
+     *    + Home: Thuận
+     *    + Search: Phương
+     *    + Schedule: Hiệp
+     *    + User: Quốc
+     *    + Settings: Bắc
+     * @note: Những chỗ code phức tạp cần comment nội dung hàm, tham số, họ tên người tạo và ngày tạo
+     * @java:
+     *    + enums: chứa các giá trị enum
+     *    + fragments: chứa các thành phần trong màn hình chính
+     *    + views: chứa các màn hình chính
+     * @res:
+     *   + drawable: chứa icon, ảnh - thêm ảnh và icon tại đây
+     *   + layout: chứa các file xml kéo thả giao diện
+     *   + values:
+     *      - color: chứa các khai báo ãm màu - thêm màu tại đây (không fix cứng)
+     *      - strings: chứa các khai báo text tiếng việt - thêm tại đây (không fix cứng)
+     *      - strings(en): chứa các khai báo text tiếng anh - thêm tại đây (không fix cứng)
+     * @git:
+     *    + Máy chưa có code muốn lấy từ github về: git clone https://github.com/nnhiep582k2/BTL_Android.git
+     *    + Khi vừa lấy code về:
+     *       - Bắt buộc tạo branch mới: git checkout -b feature/[tên] (vd: feature/nnhiep)
+     *    + Khi muốn push lên:
+     *       - git add . (thêm các thay đổi vào git local)
+     *       - git commit -m "mô tả" (xác nhận các thay đổi)
+     *       - git push origin [tên nhánh] (vd: git push origin feature/nnhiep)
+     * @author nnhiep 12.03.2023
+     */
+}
