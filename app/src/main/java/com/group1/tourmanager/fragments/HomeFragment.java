4ae74ac8fc35b1f856f28ab5216ea87481e1f2f8
quochat00 <92132035+quochat00@users.noreply.github.com>
2023-03-12 10:36:25 +0700

    sua lai

--- app/src/main/java/com/group1/tourmanager/fragments/HomeFragment.java
+++ app/src/main/java/com/group1/tourmanager/fragments/HomeFragment.java
@@ -18,8 +18,6 @@ import com.group1.tourmanager.R;
 public class HomeFragment extends Fragment {
     public HomeFragment() {}
 
-    public quoc;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
